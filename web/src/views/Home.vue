60bb64c99d72fdc0881b9ed118b32bf1444f91ac
jinlon07 <jinlon07@outlook.com>
2025-11-17 23:32:48 +0800

    Update Home.vue

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -63,7 +63,7 @@
           </svg>
           友情链接
         </button>
-        <p class="copyright">Copyright © 2025 Nav-Item | <a href="https://github.com/eooce/Nav-Item" target="_blank" class="footer-link">Powered by eooce</a></p>
+        <p class="copyright">Copyright © 2025 Nav-Item | <a href="https://https://xn--1xa.netlib.re/" target="_blank" class="footer-link">Powered by π.netlib.re </a></p>
       </div>
     </footer>
 
@@ -736,4 +736,5 @@ function handleLogoError(event) {
     gap: 20px;
   }
 }
+
 </style> 
